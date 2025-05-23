4e23fecd8d1835fa5d1004b7d0c0d808d773c353
Olga Lunevich <olga.lunevich@gmail.com>
2025-05-23 22:35:37 +0300

    feat(homework-6): Improved logic for login, logout, cancel

--- src/pages/login-page/LoginPage.jsx
+++ src/pages/login-page/LoginPage.jsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import './loginPage.css';
-import { auth} from "../../firebase.js";
-import {signInWithEmailAndPassword} from 'firebase/auth';
+import {auth, db} from "../../firebase.js";
+import { setDoc, doc } from "firebase/firestore";
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/auth';
 import {validateLoginForm} from "../../components/helpers.js";
 import {useSignOut} from "react-firebase-hooks/auth";
 
 const LoginPage = () => {
-    const [activeButton, setActiveButton] = useState('Submit');
+    const [activeButton, setActiveButton] = useState('Login');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
@@ -38,7 +39,29 @@ const LoginPage = () => {
         }
         catch (error) {
             setError({ general: error.message });
-            console.log("Something wrong during log in... error: ",error.message);
+            console.log("1 Something wrong during log in... error: ",error.message);
+            if (error.code === 'auth/user-not-found' || error.code === 'auth/invalid-credential') {
+
+                try {
+                    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                    const uid = userCredential.user.uid;
+
+                    console.log('New user created', uid);
+
+                    await setDoc(doc(db, 'users', uid), {
+                            email: email,
+                            createdAt: new Date(),
+                        }
+                    )
+                } catch (createError) {
+                    console.log("Error creating user:", createError);
+                    setError({ general: createError.message });
+                }
+
+            } else {
+                setError({ general: error.message });
+                console.log("2 Something wrong during log in... error: ",error.message);
+            }
         }
         finally {
             setLoading(false);
@@ -46,8 +69,8 @@ const LoginPage = () => {
 
     };
 
-    const handleCancelButton = async () => {
-        setActiveButton('Cancel');
+    const handleLogoutButton = async () => {
+        setActiveButton('Logout');
         setEmail('');
         setPassword('');
         setError(null);
@@ -95,13 +118,25 @@ const LoginPage = () => {
                        </div>
                        <div className="loginPageButtons">
                            <button type='submit' disabled={!email || !password}
-                               onClick={() => handleButtonClick('Submit')}
-                               className={activeButton === 'Submit' ? 'active' : ''}
-                           >Submit
+                               onClick={() => handleButtonClick('Login')}
+                               className={activeButton === 'Login' ? 'active' : ''}
+                           >Login
                            </button>
                            <button
                                type="button"
-                               onClick={() => handleCancelButton()}
+                               onClick={() => handleLogoutButton()}
+                               className={activeButton === 'Logout' ? 'active' : ''}
+                           >Logout
+                           </button>
+                           <button
+                               type="button"
+
+                               onClick={() => {
+                                   setEmail('');
+                                   setPassword('');
+                                   setError(null);
+                                   setActiveButton('Cancel');
+                               }}
                                className={activeButton === 'Cancel' ? 'active' : ''}
                            >Cancel
                            </button>
