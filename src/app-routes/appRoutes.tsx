022aac4b506cf5417f526882df069e90abe42a68
Olga Lunevich <olga.lunevich@gmail.com>
2025-06-21 23:27:12 +0300

    feat(homework-8): Refactored to Redux

--- src/app-routes/appRoutes.tsx
+++ src/app-routes/appRoutes.tsx
@@ -2,6 +2,7 @@ import {Routes, Route} from "react-router-dom";
 import HomePage from "../pages/home-page/HomePage";
 import LoginPage from "../pages/login-page/LoginPage";
 import MenuPage from "../pages/menu-page/MenuPage";
+import OrderPage from "../pages/order-page/OrderPage";
 import ProtectedRoute from "./ProtectedRout";
 import React from "react";
 
@@ -15,6 +16,7 @@ const AppRoutes= () => {
                 </ProtectedRoute>
             } />
             <Route path='/Login_page'  element={<LoginPage/>} />
+            <Route path='/OrderPage' element={<OrderPage/>} />
         </Routes>
     );
 };
